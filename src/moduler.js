d06c6af4c240d0a2a32aa56c7c2b00d2e2f00b0a
Fei Liu <liu.feiwood@gmail.com>
2014-05-02 17:32:36 +0200

    add require and define

--- src/moduler.js
+++ src/moduler.js
@@ -1,12 +1,17 @@
 var moduler = (function() {
 
+    'use strict';
+
+    var isArray = function(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    };
+
     var resolver = (function() {
 
-        'use strict';
-
         var MODULE_NAME_REGEX = /(\S+?)\.(\S+)/;
+        var MODULE_ALIAS_REGEX = /(\S+)\ as\ (\w+)/;
 
-        return function resolve(target, name, options) {
+        function resolve(target, name, options) {
 
             var parse, hasSubmodule;
             options = options || {action: 'get'};
@@ -33,30 +38,139 @@ var moduler = (function() {
                     throw new Error('Set action with an empty object.');
                 }
                 target[name] = options.obj;
+                return true;
+
             } else {
                 throw new Error('Failed to resolve.');
             }
 
+        }
+
+        function moduleName(name) {
+
+            var alias = MODULE_ALIAS_REGEX.exec(name);
+            var submodules;
+
+            if (alias) {
+                return alias[2];
+            } else {
+
+                submodules = MODULE_NAME_REGEX.exec(name);
+
+                if (submodules) {
+                    return name.split('.').pop();
+                }
+
+                return name;
+
+            }
+        }
+
+        function aliasName(name) {
+            var alias = MODULE_ALIAS_REGEX.exec(name);
+            return (alias) ? alias[1] : name;
+        }
+
+        return {
+            resolve: resolve,
+            moduleName: moduleName,
+            aliasName: aliasName
         };
 
     }());
 
-    var moduleManager = function(ns) {
-
-        ns.module = ns.module || {};
-
-        return {
-            define: function(name, fn, deps) {},
-            require: function(deps, options) {}
-        };
+    var exports = function(target, name, obj) {
+        return resolver.resolve(target, name, {action: 'set', obj: obj});
     };
 
+    var moduleManager = function(ns) {
+
+        //ns.module = ns.module || {};
+        var modules = {};
+
+        var define = function(name, fn, deps) {
+
+            var status, args = [], that = this, i, len, dep, aModule;
+
+            if (!name) {
+                throw new Error('Module name is required when defining a module.');
+            }
+
+            deps = deps || [];
+
+            if (!isArray(deps)) {
+                throw new Error('Dependencies must be supplied as an array.');
+            }
+
+            for (i = 0, len = deps.length; i < len; i++) {
+
+                dep = deps[i];
+
+                aModule = resolver.resolve(modules, dep, {action: 'get'});
+
+                if (!aModule) {
+                    console.warn('Fail to inject dependency named: "' + dep + '".');
+                }
+
+                args.push(aModule);
+            }
+
+            exports(modules, name, fn.apply(null, args));
+
+        };
+
+        var require = function(deps, options) {
+
+            var rst = {};
+
+            if (!isArray(deps)) {
+                throw new Error('Dependencies must be supplied as an array.');
+            }
+
+            // resove dependencies
+            function resolve(target, deps) {
+
+                var i, len, dep, resolvedName;
+                target = target || {};
+
+                for (i = 0, len = deps.length; i < len; i++) {
+
+                    dep = deps[i];
+
+                    // remove the dependency name, parse deep namespace or alias
+                    resolvedName = resolver.moduleName(dep);
+
+                    // assign resolved module to resolved name
+                    target[resolvedName] = resolver.resolve(modules, resolver.aliasName(dep), {action: 'get'});
+
+                    // give warning if the resolved module is empty
+                    if (typeof target[resolvedName] === 'undefined') {
+                        console.warn('module with the name "' + dep + '" is not found.');
+                    }
+
+                }
+
+                return target;
+            }
+
+            // we resovle currently empty object, if necessary we can augment exist module
+            return resolve({}, deps);
+
+        };
+
+        ns.define = define;
+        ns.require = require;
+
+    };
+
+    // api
     return {
         create: function(ns) {
             return moduleManager(ns);
         },
         exports: function(target, name, obj) {
-            return resolver(target, name, {action: 'set', obj: obj});
+            return exports(target, name, obj);
         }
     };
+
 }());
--- tests/moduler.js
+++ tests/moduler.js
@@ -25,14 +25,72 @@ describe('exports', function() {
         expect(fakeGlobal.anotherObj.some.deep.name.space.should.work.ok()).toBe('should work');
     });
 });
+
 describe('create', function() {
 
-    var foo = {bar: {}};
+    var foo;
 
-    describe('test', function() {
-        it('should work', function() {
-            moduler.create(foo.bar);
-            expect(foo.bar.module.name).toBe('module itself');
+    beforeEach(function() {
+        foo = {bar: {}};
+        moduler.create(foo.bar);
+    });
+
+    describe('the namespace should have a "define" and "require" function', function() {
+        it('should have define', function() {
+            expect(foo.bar.define).toBeDefined();
+        });
+        it('should have require', function() {
+            expect(foo.bar.require).toBeDefined();
         });
     });
+
+});
+
+describe('define and require', function() {
+
+    var foo;
+
+    beforeEach(function() {
+        foo = {};
+        moduler.create(foo);
+        foo.define('bar', function() {
+            return {
+                name: 'bar',
+                hi: function() {
+                    return this.name + ' says i\'m bar';
+                }
+            };
+        });
+    });
+
+    describe('work with simple object', function() {
+
+        it('should work', function() {
+
+            var dep = foo.require(['bar']);
+            expect(dep.bar.name).toBe('bar');
+            expect(dep.bar.hi()).toBe('bar says i\'m bar');
+
+        });
+    });
+
+    describe('depencency', function() {
+
+        it('should work', function() {
+
+            foo.define('fb', function(bar) {
+                return {
+                    name: 'fb',
+                    hi: function() {
+                        return bar.name + ' ' + this.name;
+                    }
+                };
+            }, ['bar']);
+
+            var dep = foo.require(['fb']);
+            expect(dep.fb.name).toBe('fb');
+            expect(dep.fb.hi()).toBe('bar fb');
+        });
+    });
+
 });

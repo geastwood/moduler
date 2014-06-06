69b98365c1a6a52709e473fec2732b84ef4a3439
Fei Liu <liu.feiwood@gmail.com>
2014-06-06 17:49:03 +0200

    add more modules

--- test-server/public/js/modules/module1.js
+++ test-server/public/js/modules/module1.js
@@ -2,5 +2,5 @@ moduler.define('module1', function() {
     return function(msg) {
         console.log(msg);
     };
-}, ['module0', 'module4']);
+}, ['module0', 'module4', 'module5']);
 console.log('loading module1 from server');
---
+++ test-server/public/js/modules/module5.js
@@ -0,0 +1,6 @@
+moduler.define('module5', function() {
+    return function(msg) {
+        console.log(msg);
+    };
+});
+console.log('loading module5 from server');
